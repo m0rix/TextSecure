336ce6f832a1b9886cb955495b955638df787254
Moxie Marlinspike <moxie@thoughtcrime.org>
2015-05-04 18:04:48 -0700

    Make AvatarImageView less configurable.

--- src/org/thoughtcrime/securesms/components/AvatarImageView.java
+++ src/org/thoughtcrime/securesms/components/AvatarImageView.java
@@ -2,7 +2,6 @@ package org.thoughtcrime.securesms.components;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.res.TypedArray;
 import android.graphics.Bitmap;
 import android.provider.ContactsContract;
 import android.util.AttributeSet;
@@ -13,25 +12,10 @@ import com.amulyakhare.textdrawable.TextDrawable;
 import com.amulyakhare.textdrawable.util.ColorGenerator;
 import com.makeramen.RoundedDrawable;
 
-import org.thoughtcrime.securesms.R;
 import org.thoughtcrime.securesms.contacts.ContactPhotoFactory;
 import org.thoughtcrime.securesms.recipients.Recipient;
 
 public class AvatarImageView extends ImageView {
-
-  private static final ScaleType[] SCALE_TYPES = {
-      ScaleType.MATRIX,
-      ScaleType.FIT_XY,
-      ScaleType.FIT_START,
-      ScaleType.FIT_CENTER,
-      ScaleType.FIT_END,
-      ScaleType.CENTER,
-      ScaleType.CENTER_CROP,
-      ScaleType.CENTER_INSIDE
-  };
-
-  private boolean   isOval    = false;
-  private ScaleType scaleType = ScaleType.FIT_CENTER;
 
   public AvatarImageView(Context context) {
     super(context);
@@ -39,17 +23,6 @@ public class AvatarImageView extends ImageView {
 
   public AvatarImageView(Context context, AttributeSet attrs) {
     super(context, attrs);
-
-    TypedArray styledAttributes = context.obtainStyledAttributes(attrs, R.styleable.AvatarImageView, 0, 0);
-    int        scaleTypeIndex   = styledAttributes.getInt(R.styleable.AvatarImageView_android_scaleType, -1);
-
-    if (scaleTypeIndex >= 0) {
-      this.scaleType = SCALE_TYPES[scaleTypeIndex];
-    }
-
-    this.isOval = styledAttributes.getBoolean(R.styleable.AvatarImageView_circle, false);
-
-    styledAttributes.recycle();
   }
 
   public void setAvatar(Recipient recipient, boolean quickContactEnabled) {
@@ -61,7 +34,9 @@ public class AvatarImageView extends ImageView {
     Bitmap contactPhoto = recipient.getContactPhoto();
 
     if ((contactPhoto != ContactPhotoFactory.getDefaultContactPhoto(getContext())) || (recipient.getName() == null)) {
-      setImageDrawable(RoundedDrawable.fromBitmap(contactPhoto).setScaleType(scaleType).setOval(isOval));
+      setImageDrawable(RoundedDrawable.fromBitmap(contactPhoto)
+                                      .setScaleType(ScaleType.CENTER_CROP)
+                                      .setOval(true));
     } else {
       setImageDrawable(TextDrawable.builder()
                                    .beginConfig()
